85cd67edd8beecc3f9d88f9f92366d8dfa3edc84
acer <acer@LAPTOP-0PPN85HA>
2024-11-18 12:47:30 -0600

    Agregar archivos de proyecto.

---
+++ calculadoraBasica_1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace calculadoraBasica_1
+{
+    public class Calculadora
+    {
+        public static double Operar(double num1,double num2, string OP )
+        {
+            switch (OP)
+            {
+                case "+":
+                    return num1 + num2;
+                case "-":
+                    return num1 - num2;
+                case "*":  
+                    return num1 * num2;
+                case "/":
+                    if (num2 == 0)
+                    {
+                        throw new DivideByZeroException("Error: No se puede dividir entre cero.");
+                    }
+                    return num1 / num2;
+                default:
+                    throw new ArgumentException("Error: Operador no valido.");
+            }
+        }
+    }
+}
